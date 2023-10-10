4b83b443b1651e8a638876f417cdc95d0c6e3a40
Noah-Liknaw <94854261+Noah-Liknaw@users.noreply.github.com>
2023-10-10 08:10:03 -0700

    commit

---
+++ src/content_option.js
@@ -0,0 +1,251 @@
+const logotext = '';
+const meta = {
+  title: 'Halleli',
+  description:
+    'I’m Hallelujah Endeshaw System Architect _ Full stack devloper,currently working in Ethiopia',
+};
+
+const introdata = {
+  title: 'Hi, you can call me Halle, but I introduce my self as... ',
+  animated: {
+    first: 'Full Stack Developer',
+    second: 'UX/UI Designer',
+    third: 'System Architect',
+  },
+  description: '',
+  your_img_path: 'https://avatars.githubusercontent.com/u/119032510?v=4',
+};
+
+const dataabout = {
+  title: 'abit about my self',
+  aboutme:
+    'I am a passionate and dedicated software developer with a strong enthusiasm for crafting elegant and efficient solutions to complex problems. Complexity on projects makes me feel good.',
+};
+const worktimeline = [
+  {
+    jobtitle: 'Stake',
+    where: 'Dubai, UAE(Remotly)',
+    date: '2021',
+  },
+  {
+    jobtitle: 'Azure community ',
+    where: 'Remotly',
+    date: '2020',
+  },
+  {
+    jobtitle: 'Hahu Marketing',
+    where: 'Addis Ababa, Ethiopia (Remotly)',
+    date: '2018',
+  },
+  {
+    jobtitle: 'Brit Olam',
+    where: 'Jerusalem, Israel (Remotly)',
+    date: '2020',
+  },
+  {
+    jobtitle: 'Calian(as 3rd party developer remotly) ',
+    where: 'Canada',
+    date: '2023',
+  },
+  {
+    jobtitle: 'HBGB Central',
+    where: 'Addis Ababa, Ethiopia',
+    date: '2022-2023',
+  },
+  {
+    jobtitle: 'Ethiopian Digital Farmers',
+    where: 'Addis Ababa, Ethiopia',
+    date: '2022',
+  },
+  {
+    jobtitle: 'Ethiopian Arteficial Intellegnce Institute',
+    where: 'Addis Ababa, Ethiopia',
+    date: '2021-2023',
+  },
+  {
+    jobtitle: 'Africa Interior Design',
+    where: 'Addis Ababa, Ethiopia',
+    date: '2018',
+  },
+  {
+    jobtitle: 'Chabad Vienna',
+    where: 'Vienna, Austria(Remotly)',
+    date: '2022',
+  },
+  {
+    jobtitle: 'Zergaw Cloud Service',
+    where: 'Full Stack Moblie Developer',
+    date: '2021',
+  },
+];
+
+const skills = [
+  {
+    name: 'Python',
+    value: 90,
+  },
+  {
+    name: 'C#',
+    value: 95,
+  },
+  {
+    name: 'Javascript',
+    value: 90,
+  },
+  {
+    name: 'React',
+    value: 90,
+  },
+  {
+    name: 'Jquery',
+    value: 85,
+  },
+  {
+    name: 'Next.js',
+    value: 70,
+  },
+  {
+    name: 'Flutter',
+    value: 80,
+  },
+  {
+    name: 'Dart',
+    value: 85,
+  },
+  {
+    name: 'Fortran',
+    value: 99,
+  },
+  {
+    name: 'Java',
+    value: 85,
+  },
+  {
+    name: 'Figma',
+    value: 90,
+  },
+
+  {
+    name: 'Laravel',
+    value: 85,
+  },
+  {
+    name: 'SQL(Mysql, Postgres)',
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: 'UI & UX Design',
+    description:
+      'Starting form scratch to the reality. I mostly use Figma, Framer and Sketch.',
+  },
+  {
+    title: 'Mobile Apps',
+    description:
+      'I can develop for both android and ios apps with enterprize levels of kotlin, swift, flutter.',
+  },
+  {
+    title: 'Software Architect',
+    description:
+      'With my experiance at enterprize level I design Clean, Hexagonal and onion software architecture',
+  },
+];
+
+const dataportfolio = [
+  {
+    img: 'https://img.freepik.com/premium-vector/mobile-shopping-concept-product-catalog-mobile-app-with-discount-coupons_530733-1239.jpg?size=626&ext=jpg&ga=GA1.2.628017531.1680208823&semt=ais',
+    description: 'This project was made for Uniliver by using Figma.',
+    link: 'https://www.figma.com/file/z7hti0c7TZuPkzU1Hu8qtr/UNILI?type=design&node-id=0%3A1&mode=design&t=NiPhTRYuNVp97Zqu-1',
+  },
+  {
+    img: 'https://raw.githubusercontent.com/nati589/elearning/a88c77f1d300b796e71dc9e804c1985579192f65/client/src/assets/login_image.svg',
+    description: 'E-learning Platform',
+    link: 'https://www.figma.com/file/Udzi0eHsOR2a87d89L9l71/Untitled?type=design&node-id=0%3A1&mode=design&t=v4AemxJtLT2D58WP-1',
+  },
+  {
+    img: 'https://img.freepik.com/free-vector/mobile-apps-concept-illustration_114360-690.jpg?size=626&ext=jpg&ga=GA1.1.628017531.1680208823&semt=ais',
+    description: 'My Social Midea Design',
+    link: 'https://www.figma.com/file/YVfVxj6QuYmAZ9uOUNwvYJ/my-social-Bezal?type=design&node-id=2%3A1005&mode=design&t=WsXL9ecfZT2TeBBc-1',
+  },
+  {
+    img: 'https://img.freepik.com/free-vector/hand-drawn-flat-design-sql-illustration_23-2149245997.jpg?size=626&ext=jpg&ga=GA1.2.628017531.1680208823&semt=ais',
+    description:
+      'Onion Architecture for enterprise company using waterfall stratagy',
+    link: 'https://github.com/Halleendod/Onioinarchitecture.git',
+  },
+  {
+    img: 'https://img.freepik.com/free-vector/tiny-people-analyst-data-scientist-working-with-data-data-driven-business-model-comprehensive-data-strategies-new-economic-model-concept_335657-2420.jpg?size=626&ext=jpg&ga=GA1.2.628017531.1680208823&semt=ais',
+    description: 'Website Design by wordpress and mysql.',
+    link: 'https://chabadvienna.com/en/',
+  },
+  {
+    img: 'https://img.freepik.com/free-vector/order-ride-concept-illustration_114360-5095.jpg?size=626&ext=jpg&ga=GA1.1.628017531.1680208823&semt=ais',
+    description: 'Uber type transportation app platform.',
+    link: 'https://feres.et/',
+  },
+
+  {
+    img: 'https://img.freepik.com/free-vector/www-concept-illustration_114360-2143.jpg?size=626&ext=jpg&ga=GA1.1.628017531.1680208823&semt=sph',
+    description:
+      'The wisdom of life consists in the elimination of non-essentials.',
+    link: '#',
+  },
+  {
+    img: 'https://img.freepik.com/free-vector/website-designer-concept-illustration_114360-4449.jpg?size=626&ext=jpg&ga=GA1.2.628017531.1680208823&semt=ais',
+    description: 'UX and UI design for ICC',
+    link: 'https://www.figma.com/file/XzRWo1zKuh0No9loYrKCOZ/Landing-Page-for-ICC?type=design&node-id=0%3A1&mode=design&t=AGkv09AZMlHCMwvO-1',
+  },
+  {
+    img: 'https://img.freepik.com/free-vector/customer-support-abstract-concept_335657-3000.jpg?size=626&ext=jpg&ga=GA1.2.628017531.1680208823&semt=ais',
+    description: 'Sales Management System ui for African Interior Design',
+    link: 'https://www.figma.com/file/H9J6lz05lQTPSz3V1BE5vQ/Sales-Management-System-ui-for?type=design&node-id=0%3A1&mode=design&t=2qKmMqc1SUp3EDkb-1',
+  },
+  {
+    img: 'https://img.freepik.com/free-vector/memory-storage-concept-illustration_114360-2942.jpg?size=626&ext=jpg&ga=GA1.2.628017531.1680208823&semt=ais',
+    description: 'Cloud Storage for all.',
+    link: 'https://apkpure.com/cloud-storage-backup-files/com.cloud.storage.driveapp.photo.storage',
+  },
+  {
+    img: 'https://img.freepik.com/free-vector/hotel-booking-concept-illustration_114360-6497.jpg?size=626&ext=jpg&ga=GA1.2.628017531.1680208823&semt=ais',
+    description: 'Traveling app for mister of tourism.',
+    link: 'https://apkpure.com/nl/visit-ethiopia/com.essti.visitethiopia',
+  },
+  {
+    img: 'https://img.freepik.com/free-vector/real-estate-searching-with-phone_23-2148654927.jpg?size=626&ext=jpg&ga=GA1.1.628017531.1680208823&semt=ais',
+    description: 'Real State investing app in Dubai.',
+    link: 'https://img.freepik.com/free-vector/real-estate-searching-with-phone_23-2148654927.jpg?size=626&ext=jpg&ga=GA1.1.628017531.1680208823&semt=ais',
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: 'hallelujahendeshawt@gmail.com',
+  YOUR_FONE: '+251 962188710',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ',
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: 'service_wk0ccy9',
+  YOUR_TEMPLATE_ID: 'template_39nsdij',
+  YOUR_USER_ID: 'ug1sE_yr0_bCJ4mEg',
+};
+
+const socialprofils = {
+  github: 'https://github.com/Halleendod',
+  facebook: 'https://facebook.com',
+  linkedin: 'https://linkedin.com',
+  twitter: 'https://twitter.com',
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
